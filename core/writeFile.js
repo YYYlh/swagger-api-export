354c884dd5b78806f780bb44bdd48b9ba497b29c
liuhao <liuhao@qq.com>
2020-03-05 11:19:20 +0800

    能用就行

--- core/writeFile.js
+++ core/writeFile.js
@@ -12,30 +12,23 @@ function format(str) {
 }
 // 在这里写入文件
 exports.writeConfigFile = function(fileData) {
+  const fileName = 'config.js'
   const formatName = format(fileData.serverName)
   const content = `export const ${formatName} = 'http://${fileData.serverUrl}/${fileData.serverName}'\n`
   // 在写入时先获取文件的内容
-  fs.readFile('config.js', (err, data) => {
+  fs.readFile(fileName, (err, data) => {
     if (err) {
-      wirte(content)
+      wirte(fileName, content)
     } else {
       // 如果文件中存在相同数据-则不需要在写入
       if (String(data).includes(content)) return false
-      wirte(content)
+      wirte(fileName, content)
     }
   })
-  function wirte(content) {
-    fs.appendFile('config.js', content, (error) => {
-      if(error){
-        console.log(error);
-        return false;
-      }
-      console.log('\x1B[32m%s\x1b[39m', `config.js写入成功 👌`);
-    })
-  }
 }
 exports.writeRestUrlFile = function(fileData) {
   let str = ''
+  const fileName = `${fileData.serverName}-url.js`
   const formatName = format(fileData.serverName)
   for (const key in fileData.paths) {
     let pathStr = ''
@@ -62,31 +55,31 @@ ${str}
 }
 ` 
 // ====== 拼接字符串区域 end===============
-  fs.readFile(`${fileData.serverName}-url.js`, (err, data) => {
+  fs.readFile(fileName, (err, data) => {
     content = deleteFirstRow(content)
     if (err) {
-      wirte(content)
+      wirte(fileName, content)
     } else if(!String(data)) {
-      wirte(content)
+      wirte(fileName, content)
     } else {
       // 如果不是第一次写入 在原先的基础上追加
       // 原先内容
       const oldContentArr = String(data).split('\n')
       oldContentArr.splice(oldContentArr.length - 2, 0, str)
-      wirte(oldContentArr.join('\n'))
+      wirte(fileName, oldContentArr.join('\n'))
     }
   })
-  function wirte(content) {
-    fs.writeFile(`${fileData.serverName}-url.js`, content, (error) => {
-      if(error){
-          console.log(error);
-          return false;
-      }
-      console.log('\x1B[32m%s\x1b[39m', `${fileData.serverName}-url.js写入成功 👌`);
-    })
-  }
 }
-
+// 写入文件
+function wirte(fileName, content) {
+  fs.writeFile(fileName, content, (error) => {
+    if(error){
+        console.log(error);
+        return false;
+    }
+    console.log('\x1B[32m%s\x1b[39m', `${fileName} -- 👌`);
+  })
+}
 // 删除文件内容的第一行
 function deleteFirstRow(data) {
   const arr = String(data).split('\n')
