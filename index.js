8d23ede5564abd4c1e7eb1c096d14bc384b71fe4
YYYlh <1184262331@qq.com>
2020-08-09 20:48:00 +0800

    重写完毕

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 #!/usr/bin/env node
-require('./global')() // 添加全局变量(log)
-const program = require('commander')
+require('./lib/global')() // 添加全局变量(log)
+const FetchSwagger = require('./dist/fetch_swagger')
+const { program } = require('commander')
+const process = require('process')
 
+program
+    .version('1.0.0')
+
+program
+    .command('fetch [serverUrl] [controllers...]')
+    .description('读取swagger地址')
+    .action(function(serverUrl, controllers){
+        new FetchSwagger(serverUrl, controllers)
+    })
+program.parse(process.argv)
 
  
